9786daf3ca6de0f3cccf487d627256015e639711
liscanojose <jose87javier@gmail.com>
2020-08-02 18:58:24 -0400

    docs2

---
+++ docs2/precache-manifest.dccda68ddd4a471603cf8e5a01d0fd88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b031bbd9ec95960f5289a9c09227e509",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33cf23808695760ef71a",
+    "url": "/static/css/main.b93cf9db.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33cf23808695760ef71a",
+    "url": "/static/js/main.a7d4439a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
